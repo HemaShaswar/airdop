5cb1fb268232de245cf0019e1158687fec4c26df
hema <shaswarhema@gmail.com>
2024-07-03 20:35:34 +0300

    added the wallets

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-simple solana library experiments
+Final Project result TX link:
+https://explorer.solana.com/tx/5RCi8XxzCn1tsDSD7mdtvHeDgRn5Sv5xn7AkgsxSVyKzChVjj8VNCydqAXEsVrLMwDSzqvpqzrZHwZZTbnbT7tWu?cluster=devnet
+
+keygen.ts creates a new solana keypair and logs it to the console
+yarn keygen
+
+airdrop.ts requests 2 sol airdrop
+airdrop.ts
+
+yarn airdrop
+yarn transfer
+yarn enroll
--- programs/wba_prereq.ts
+++ programs/wba_prereq.ts
@@ -1,204 +1,277 @@
 export type WbaPrereq = {
-  name: "wba_prereq",
-  version: "0.1.0",
-  spec: "0.1.0",
-  description: "Created with Anchor",
-
-  instructions: [
+  "address": "WBA52hW35HZU5R2swG57oehbN2fTr7nNhNDgfjnqUoZ",
+  "metadata": {
+    "name": "wba_prereq",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: "complete",
-      discriminator: [0, 77, 224, 147, 136, 25, 88, 76],
-      accounts: [
+      "name": "complete",
+      "discriminator": [
+        0,
+        77,
+        224,
+        147,
+        136,
+        25,
+        88,
+        76
+      ],
+      "accounts": [
         {
-          name: "signer",
-          writable: true,
-          signer: true,
+          "name": "signer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: "prereq",
-          writable: true,
-          pda: {
-            seeds: [
+          "name": "prereq",
+          "writable": true,
+          "pda": {
+            "seeds": [
               {
-                kind: "const",
-                value: [112, 114, 101, 114, 101, 113],
+                "kind": "const",
+                "value": [
+                  112,
+                  114,
+                  101,
+                  114,
+                  101,
+                  113
+                ]
               },
               {
-                kind: "account",
-                path: "signer",
+                "kind": "account",
+                "path": "signer"
               }
-            ],
-          },
+            ]
+          }
         },
         {
-          name: "system_program",
-          address: "11111111111111111111111111111111",
+          "name": "system_program",
+          "address": "11111111111111111111111111111111"
         }
       ],
-      args: [
+      "args": [
         {
-          name: "github",
-          type: "bytes",
+          "name": "github",
+          "type": "bytes"
         }
-      ],
+      ]
     },
     {
-      name: "update",
-      discriminator: [219, 200, 88, 176, 158, 63, 253, 127],
-      accounts: [
+      "name": "update",
+      "discriminator": [
+        219,
+        200,
+        88,
+        176,
+        158,
+        63,
+        253,
+        127
+      ],
+      "accounts": [
         {
-          name: "signer",
-          writable: true,
-          signer: true,
+          "name": "signer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: "prereq",
-          writable: true,
+          "name": "prereq",
+          "writable": true
         },
         {
-          name: "system_program",
-          address: "11111111111111111111111111111111",
+          "name": "system_program",
+          "address": "11111111111111111111111111111111"
         }
       ],
-      args: [
+      "args": [
         {
-          name: "github",
-          type: "bytes",
+          "name": "github",
+          "type": "bytes"
         }
-      ],
+      ]
     }
   ],
-  accounts: [
+  "accounts": [
     {
-      name: "Q2Prereq2024",
-      discriminator: [210, 203, 168, 103, 251, 233, 204, 6],
+      "name": "Q2Prereq2024",
+      "discriminator": [
+        210,
+        203,
+        168,
+        103,
+        251,
+        233,
+        204,
+        6
+      ]
     }
   ],
-  errors: [
+  "errors": [
     {
-      code: 6000,
-      name: "InvalidGithubAccount",
-      msg: "Invalid Github account",
+      "code": 6000,
+      "name": "InvalidGithubAccount",
+      "msg": "Invalid Github account"
     }
   ],
-  types: [
+  "types": [
     {
-      name: "Q2Prereq2024",
-      type: {
-        kind: "struct",
-        fields: [
+      "name": "Q2Prereq2024",
+      "type": {
+        "kind": "struct",
+        "fields": [
           {
-            name: "github",
-            type: "bytes",
+            "name": "github",
+            "type": "bytes"
           },
           {
-            name: "key",
-            type: "pubkey",
+            "name": "key",
+            "type": "pubkey"
           }
-        ],
-      },
+        ]
+      }
     }
-  ],
+  ]
 };
 
 export const IDL: WbaPrereq = {
-  name: "wba_prereq",
-  version: "0.1.0",
-  spec: "0.1.0",
-  description: "Created with Anchor",
-  instructions: [
+  "address": "WBA52hW35HZU5R2swG57oehbN2fTr7nNhNDgfjnqUoZ",
+  "metadata": {
+    "name": "wba_prereq",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: "complete",
-      discriminator: [0, 77, 224, 147, 136, 25, 88, 76],
-      accounts: [
+      "name": "complete",
+      "discriminator": [
+        0,
+        77,
+        224,
+        147,
+        136,
+        25,
+        88,
+        76
+      ],
+      "accounts": [
         {
-          name: "signer",
-          writable: true,
-          signer: true,
+          "name": "signer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: "prereq",
-          writable: true,
-          pda: {
-            seeds: [
+          "name": "prereq",
+          "writable": true,
+          "pda": {
+            "seeds": [
               {
-                kind: "const",
-                value: [112, 114, 101, 114, 101, 113],
+                "kind": "const",
+                "value": [
+                  112,
+                  114,
+                  101,
+                  114,
+                  101,
+                  113
+                ]
               },
               {
-                kind: "account",
-                path: "signer",
-              },
-            ],
-          },
+                "kind": "account",
+                "path": "signer"
+              }
+            ]
+          }
         },
         {
-          name: "system_program",
-          address: "11111111111111111111111111111111",
-        },
+          "name": "system_program",
+          "address": "11111111111111111111111111111111"
+        }
       ],
-      args: [
+      "args": [
         {
-          name: "github",
-          type: "bytes",
-        },
-      ],
+          "name": "github",
+          "type": "bytes"
+        }
+      ]
     },
     {
-      name: "update",
-      discriminator: [219, 200, 88, 176, 158, 63, 253, 127],
-      accounts: [
-        {
-          name: "signer",
-          writable: true,
-          signer: true,
-        },
-        {
-          name: "prereq",
-          writable: true,
-        },
-        {
-          name: "system_program",
-          address: "11111111111111111111111111111111",
-        },
+      "name": "update",
+      "discriminator": [
+        219,
+        200,
+        88,
+        176,
+        158,
+        63,
+        253,
+        127
       ],
-      args: [
+      "accounts": [
         {
-          name: "github",
-          type: "bytes",
+          "name": "signer",
+          "writable": true,
+          "signer": true
         },
+        {
+          "name": "prereq",
+          "writable": true
+        },
+        {
+          "name": "system_program",
+          "address": "11111111111111111111111111111111"
+        }
       ],
-    },
+      "args": [
+        {
+          "name": "github",
+          "type": "bytes"
+        }
+      ]
+    }
   ],
-  accounts: [
+  "accounts": [
     {
-      name: "Q2Prereq2024",
-      discriminator: [210, 203, 168, 103, 251, 233, 204, 6],
-    },
+      "name": "Q2Prereq2024",
+      "discriminator": [
+        210,
+        203,
+        168,
+        103,
+        251,
+        233,
+        204,
+        6
+      ]
+    }
   ],
-  errors: [
+  "errors": [
     {
-      code: 6000,
-      name: "InvalidGithubAccount",
-      msg: "Invalid Github account",
-    },
+      "code": 6000,
+      "name": "InvalidGithubAccount",
+      "msg": "Invalid Github account"
+    }
   ],
-  types: [
+  "types": [
     {
-      name: "Q2Prereq2024",
-      type: {
-        kind: "struct",
-        fields: [
+      "name": "Q2Prereq2024",
+      "type": {
+        "kind": "struct",
+        "fields": [
           {
-            name: "github",
-            type: "bytes",
+            "name": "github",
+            "type": "bytes"
           },
           {
-            name: "key",
-            type: "pubkey",
-          },
-        ],
-      },
-    },
-  ],
+            "name": "key",
+            "type": "pubkey"
+          }
+        ]
+      }
+    }
+  ]
 }
